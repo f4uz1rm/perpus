f830e978fb0183ee70bbf13af2b3104ce1f86bc6
Fauzi Rizky Mauladani <f4uz1rm@gmail.com>
2024-01-19 21:37:11 +0700

    Pertama kali push

---
+++ resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,105 @@
+<nav class="sidebar">
+    <div class="sidebar-header">
+        <a href="#" class="sidebar-brand">
+            Perpustakaan
+        </a>
+        <div class="sidebar-toggler not-active">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="sidebar-body">
+        <ul class="nav">
+            <li class="nav-item nav-category">Menu</li>
+            <li class="nav-item">
+                <a href="dashboard.html" class="nav-link">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="link-title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item nav-category">Transaksi</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#transaksi" role="button" aria-expanded="false"
+                    aria-controls="transaksi">
+                    <i class="link-icon" data-feather="mail"></i>
+                    <span class="link-title">Transaksi</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="transaksi">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="pages/email/inbox.html" class="nav-link">Peminjaman Buku</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/email/read.html" class="nav-link">Pengembalian Buku</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/email/read.html" class="nav-link">Denda</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+              <a href="pages/apps/chat.html" class="nav-link">
+                  <i class="link-icon" data-feather="calendar"></i>
+                  <span class="link-title">Jadwal Kunjungan</span>
+              </a>
+          </li>
+          <li class="nav-item nav-category">Master</li>
+            <li class="nav-item">
+                <a href="pages/apps/chat.html" class="nav-link">
+                    <i class="link-icon" data-feather="book-open"></i>
+                    <span class="link-title">Data Buku</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/apps/chat.html" class="nav-link">
+                    <i class="link-icon" data-feather="bookmark"></i>
+                    <span class="link-title">Kategori Buku</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/apps/chat.html" class="nav-link">
+                    <i class="link-icon" data-feather="users"></i>
+                    <span class="link-title">Data Anggota</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/apps/chat.html" class="nav-link">
+                    <i class="link-icon" data-feather="user-check"></i>
+                    <span class="link-title">Data Pengunjung</span>
+                </a>
+            </li>
+          
+
+            <li class="nav-item nav-category">Laporan</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#laporan" role="button" aria-expanded="false"
+                    aria-controls="laporan">
+                    <i class="link-icon" data-feather="printer"></i>
+                    <span class="link-title">Laporan</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="laporan">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="pages/email/inbox.html" class="nav-link">Keuangan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/email/read.html" class="nav-link">Peminjaman / Pengembalian</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/email/read.html" class="nav-link">Buku</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/email/read.html" class="nav-link">Pengunjung</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
