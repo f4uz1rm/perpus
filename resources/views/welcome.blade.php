104a90ba828afb90a4be9081a3e016cfb5fe3368
Fauzi Rizky Mauladani <f4uz1rm@gmail.com>
2024-01-30 05:44:19 +0700

    Welcome Web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Welcome</title>
+        <title>Laravel Welcome Web</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
